49834100dd6e2a540455c9a7590982d7af43cf6f
Shymolly <89173233+Shymolly@users.noreply.github.com>
2021-10-27 16:56:01 -0500

    Add files via upload

---
+++ Prototipo_SDSF/Prototipo/Prototipo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prototipo
+//
+//  Created by AlumnoUTM on 3/4/20.
+//  Copyright © 2020 usuario invitado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Prototipo_SDSF/Prototipo/ViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Realice cualquier configuración adicional después de cargar la vista, generalmente desde una plumilla.
+    }
+
+
+}
+
